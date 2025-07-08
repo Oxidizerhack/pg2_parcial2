a153c56b37ec7221b83a789ab3d24ce16e2422fb
Jhonny] <jhonnyantoquispe@gmail.com>
2025-07-07 23:14:36 -0400

    pg2_parcial2

--- api_conos/admin.py
+++ api_conos/admin.py
@@ -1,4 +1,68 @@
 from django.contrib import admin
-from .models import PedidoCono
+from django import forms
+from .models import PedidoCono, TOPPINGS_VALIDOS
 
-admin.site.register(PedidoCono)
+class PedidoConoForm(forms.ModelForm):
+    # Campo personalizado para toppings
+    toppings_seleccionados = forms.MultipleChoiceField(
+        choices=[(t, t) for t in TOPPINGS_VALIDOS],
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+        label="Toppings (selecciona los que quieras)"
+    )
+    
+    class Meta:
+        model = PedidoCono
+        fields = '__all__'
+        widgets = {
+            'toppings': forms.HiddenInput(),  # Ocultar el campo JSON
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Si estamos editando, mostrar toppings seleccionados
+        if self.instance and self.instance.pk:
+            self.fields['toppings_seleccionados'].initial = self.instance.toppings or []
+    
+    def save(self, commit=True):
+        # Convertir selección de checkboxes a JSON
+        instance = super().save(commit=False)
+        instance.toppings = list(self.cleaned_data['toppings_seleccionados'])
+        if commit:
+            instance.save()
+        return instance
+
+@admin.register(PedidoCono)
+class PedidoConoAdmin(admin.ModelAdmin):
+    form = PedidoConoForm
+    list_display = ['cliente', 'variante', 'tamanio_cono', 'fecha_pedido', 'get_toppings_display']
+    list_filter = ['variante', 'tamanio_cono', 'fecha_pedido']
+    search_fields = ['cliente']
+    readonly_fields = ['fecha_pedido']
+    
+    fieldsets = (
+        ('Información del Cliente', {
+            'fields': ('cliente',)
+        }),
+        ('Detalles del Pedido', {
+            'fields': ('variante', 'tamanio_cono', 'toppings_seleccionados')
+        }),
+        ('Información del Sistema', {
+            'fields': ('fecha_pedido',),
+            'classes': ('collapse',)
+        }),
+    )
+    
+    def get_toppings_display(self, obj):
+        toppings = obj.toppings or []
+        if not toppings:
+            return "Sin toppings"
+        return ", ".join(toppings)
+    get_toppings_display.short_description = 'Toppings'
+    
+    def save_model(self, request, obj, form, change):
+        try:
+            super().save_model(request, obj, form, change)
+            self.message_user(request, f'Pedido para {obj.cliente} guardado exitosamente.')
+        except Exception as e:
+            self.message_user(request, f'Error al guardar: {str(e)}', level='ERROR')
--- api_patrones/urls.py
+++ api_patrones/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/conos/', include('api_conos.urls')),
+    path('api/', include('api_conos.urls')),
 ]
